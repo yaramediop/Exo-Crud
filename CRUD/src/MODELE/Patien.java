0f0067ae4affeb930d991dcf8bf9f7e748049fed
mamediarrandiayegw-cloud <mamediarrandiayegw@gmail.com>
2026-03-02 11:51:05 +0000

    diarra
    diarra

--- CRUD/src/MODELE/Patien.java
+++ CRUD/src/MODELE/Patien.java
@@ -1,5 +1,50 @@
 package MODELE;
 
 public class Patien {
+private int idP;
+private String nom;
+private String prenom;
+private String sexe;
+private int age;
 
+public patient (){}
+public patient (String nom, String prenom, String sexe, int age){
+    this.nom= nom;
+    this.prenom= prenom;
+    this.sexe= sexe;
+    this.age= age;
+}
+   // Getters
+    public int getIdP() {
+        return idP;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public String getSexe() {
+        return sexe;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    // Setters
+    public void setIdP(int idP) {
+        this.idP = idP;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
