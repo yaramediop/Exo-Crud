409c3ec2294bb8bd878062df9d811cf9b8772af1
fama62787-ai <fama62787@gmail.com>
2026-03-02 11:53:00 +0000

    fama
    fama

--- CRUD/src/MODELE/Medecin.java
+++ CRUD/src/MODELE/Medecin.java
@@ -3,10 +3,39 @@ package MODELE;
 public class Medecin {
 private int id;
 private String nom;
-public Medecin(int id, String nom) {
+private String prenom;
+private String specialite;
+public Medecin(int id, String nom , String prenom, String specialite) {
 this.id = id;
 this.nom = nom;
+this.prenom = prenom;
+this.specialite = specialite;
+}
+public int getId() {
+return id;
+}
+public void setId(int id) {
+this.id = id;
+}
+public String getNom() {
+return nom;
+}
 
+public void setNom(String nom) {
+this.nom = nom;
+}
 
+public void setPrenom(String prenom) {
+this.prenom = prenom;
+}
+public String getPrenom() {
+return prenom;
+}
+
+public String getSpecialite() {
+return specialite;
+}
+public void setSpecialite(String specialite) {
+this.specialite = specialite;
 }
 }
