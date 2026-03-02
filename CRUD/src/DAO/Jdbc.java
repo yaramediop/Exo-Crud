aaf474145ecd6e454019855a4bd4be48f5bc2371
yaramediop <yaramediop806@gmail.com>
2026-03-02 11:55:14 +0000

    daccb
    tyjj

--- CRUD/src/DAO/Jdbc.java
+++ CRUD/src/DAO/Jdbc.java
@@ -1,5 +1,36 @@
-package DAO;
+package Dao;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Jdbc {
-
+	String URL="jdbc:mysql://localhost/MVC";
+	String Driver="com.mysql.cj.jdbc.Driver";
+	String user="root";
+	String Password="";
+	// Import java connection(api java.sql)
+	public Connection co= null;
+	public Statement sta= null;
+	// Methode contenant l'ensemble des actions à réaliser
+	public void connect() {
+	
+	// gérer les exceptions
+		try {
+	//1etape: chargement du driver avec la methode Forname
+		Class.forName(Driver);
+		
+	//2etape: connection à la base
+	  co=DriverManager.getConnection(URL,user,Password);
+	  
+	//3etape: statement		
+     sta=co.createStatement();
+    		 
+ 		 
+     
+	}
+	catch(Exception e) {
+	e.printStackTrace();
+	}
+}
 }
